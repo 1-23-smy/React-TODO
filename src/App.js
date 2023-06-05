747ae87bca0fb6f1f25e38e42952dfa3f66e8f3e
Soumya Ranjan Das <soumyaranjandas@Soumyas-MacBook-Air.local>
2023-06-05 09:43:26 +0530

    Added TODO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  Input,
+  HStack,
+  VStack,
+  Box,
+  ListItem,
+  UnorderedList,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { RiDeleteBin7Line } from "react-icons/ri";
+const App = () => {
+  const [activity, setActivity] = useState("");
+  const [listData, setListData] = useState([]);
+  const addHandler = () => {
+    setListData((listData) => {
+      const updatedList = [...listData, activity];
+      console.log(updatedList);
+      setActivity("");
+      return updatedList;
+    });
+  };
+  const RemoveElement = (i) => {
+    const updatedListData = listData.filter((elem, id) => {
+      return i !== id;
+    });
+    setListData(updatedListData);
+  };
+  const Removeall = () => {
+    setListData([]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container mt="8" py='16'>
+      <Heading
+        children="my TodoList"
+        textTransform={"uppercase"}
+        textAlign={"center"}
+        fontFamily={"mono"}
+      />
+      <HStack mt={"4"}>
+        <Input
+          type="text"
+          placeholder="Enter here"
+          value={activity}
+          onChange={(e) => setActivity(e.target.value)}
+        />
+        <Button
+          size={"md"}
+          variant={"outline"}
+          colorScheme="yellow"
+          onClick={() => addHandler()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Enter to add
+        </Button>
+        
+      </HStack>
+      <Text
+        children="Your list is here"
+        textAlign={"center"}
+        fontSize={"xl"}
+        fontFamily={"monospace"}
+        mt="4"
+      />
+      <Box h={"fit-content"} boxShadow={"lg"} mt="3" borderRadius={'2xl'}>
+        <VStack>
+          {listData.map((el, i) => (
+            <HStack key={i}>
+              <UnorderedList>
+                <ListItem
+                  textColor={"ThreeDShadow"}
+                  fontFamily={"monospace"}
+                  fontSize={"xl"}
+                  fontWeight={"extrabold"}
+                >
+                  {el}
+                </ListItem>
+              </UnorderedList>
+
+              <Button
+                size={"sm"}
+                variant={"ghost"}
+                colorScheme="purple"
+                onClick={() => RemoveElement(i)}
+              >
+                <RiDeleteBin7Line />
+              </Button>
+            </HStack>
+          ))}
+          {
+            listData.length >=1 && <Button colorScheme='purple' variant='solid' mb='2' onClick={()=>Removeall()} borderRadius={'2xl'}>
+            Remove all
+            </Button>
+          }
+        </VStack>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
